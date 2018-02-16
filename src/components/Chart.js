f0e9bb384bd13578b95fd0b88069ad8179f3f9f5
Sam Xie <samxie@outlook.com.au>
2018-02-17 04:04:25 +0800

    Revert "Single redraw, improved responsiveness"
    This reverts commit 3d933833518b07861ac84acd87b5eed829e6c5ea.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -9,6 +9,11 @@ class Chart extends Card {
     componentDidMount(){
         window.addEventListener('resize', () => {
             this.setState(() => ({ redraw: true}));
+            for(var i=0; i<10;i++){
+                setTimeout(() => {
+                    this.setState(() => ({ redraw: true }));
+                }, 100*i);
+            }
         });
     }
     
